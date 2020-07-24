6fe2bc7f2958a74525ce8636a48b78847e8a2122
Raghu Betina <raghu@firstdraft.com>
2020-07-24 15:20:52 +0000

    Generate graphiti resources for castings

---
+++ app/resources/casting_resource.rb
@@ -0,0 +1,15 @@
+class CastingResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :movie_id, :integer
+  attribute :actor_id, :integer
+  attribute :character_name, :string
+  attribute :profile, :string
+  attribute :addr, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
